45ad05a6bd5971cb348ccf97981a61b189086bab
RichardStelzer <40047008+RichardStelzer@users.noreply.github.com>
2022-11-18 22:56:52 +0100

    Added results page.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.http import Http404
